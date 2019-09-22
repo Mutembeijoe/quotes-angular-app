1a239b64ef60c00aa858ec7f28ea791f04ce6e38
mutembeijoe <mutembeijoe@gmail.com>
2019-09-22 11:37:12 +0300

    changed environment to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCDs9tRsCl5xwSU5xzkWmsNK_PSfD-o_hk',
+    authDomain: 'quotes-d34be.firebaseapp.com',
+    databaseURL: 'https://quotes-d34be.firebaseio.com',
+    projectId: 'quotes-d34be',
+    storageBucket: '',
+    messagingSenderId: '756349198541',
+    appId: '1:756349198541:web:c2dd97e1a775d0e170c984'
+  }
 };
